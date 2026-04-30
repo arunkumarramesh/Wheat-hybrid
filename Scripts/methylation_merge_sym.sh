31235709174dbab5d19c91d59dc2f4ae25aba3f8
Arunkumar Ramesh <arunkumarramrsh@gmail.com>
2026-04-30 22:11:05 +0100

    Create methylation_merge_sym.sh

---
+++ Scripts/methylation_merge_sym.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+f1="$1"
+f2="$2"
+f3="$3"
+out="$4"
+
+SEP=$'\034'
+
+LC_ALL=C join -t $'\t' \
+    <(
+        gzip -dc "$f1" | awk -v OFS='\t' -v sep="$SEP" '
+        {
+            cov = $3 + $4
+            if (cov > 2)
+                printf "%s%s%s\t%.6f\t%d\n", $1, sep, $2, ($3/cov)*100, cov
+        }'
+    ) \
+    <(
+        gzip -dc "$f2" | awk -v OFS='\t' -v sep="$SEP" '
+        {
+            cov = $3 + $4
+            if (cov > 2)
+                printf "%s%s%s\t%.6f\t%d\n", $1, sep, $2, ($3/cov)*100, cov
+        }'
+    ) |
+LC_ALL=C join -t $'\t' - \
+    <(
+        gzip -dc "$f3" | awk -v OFS='\t' -v sep="$SEP" '
+        {
+            cov = $3 + $4
+            if (cov > 2)
+                printf "%s%s%s\t%.6f\t%d\n", $1, sep, $2, ($3/cov)*100, cov
+        }'
+    ) |
+awk -v OFS='\t' -v sep="$SEP" '
+BEGIN {
+    print "chr","pos","pct_CS","cov_CS","pct_CSxP","cov_CSxP","pct_P","cov_P"
+}
+{
+    split($1, a, sep)
+    print a[1], a[2], $2, $3, $4, $5, $6, $7
+}' | gzip > "$out"
