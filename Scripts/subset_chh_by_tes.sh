fc941bcf1cf6d2a010a8afc4a5ec247f539c9cc2
Arunkumar Ramesh <arunkumarramrsh@gmail.com>
2026-06-01 14:12:04 +0100

    Create subset_chh_by_tes.sh

---
+++ Scripts/subset_chh_by_tes.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+INPUT="$1"
+OUTPUT="$2"
+TE_BED="TEs.bed"
+
+(
+printf "chr\tstart\tend\tte_id\tte_strand\tte_class\tte_family\tte_consensus\tte_consensus_pct\tte_status\tte_copie\tte_compo\tte_post\tte_length\tsubgenome\tnearest_gene_id\tdistance_to_gene\tcentromere_status\tcentromere_interval\tn_sites\tpct_CS\tcov_CS\tpct_CSxP\tcov_CSxP\tpct_P\tcov_P\n"
+
+zcat "$INPUT" |
+awk 'BEGIN{FS=OFS="\t"} NR>1{print $1,$2-1,$2,$0}' |
+bedtools intersect -a stdin -b "$TE_BED" -wa -wb |
+awk 'BEGIN{FS=OFS="\t"}
+{
+    key=$13 OFS $14 OFS $15 OFS $16 OFS $17 OFS $18 OFS $19 OFS $20 OFS $21 OFS $22 OFS $23 OFS $24 OFS $25 OFS $26 OFS $27 OFS $28 OFS $29 OFS $30 OFS $31
+
+    if(!(key in seen)){
+        seen[key]=1
+        order[++n]=key
+    }
+
+    nsites[key]++
+
+    meth_CS[key]+=$7*$8/100
+    cov_CS[key]+=$8
+
+    meth_CSxP[key]+=$9*$10/100
+    cov_CSxP[key]+=$10
+
+    meth_P[key]+=$11*$12/100
+    cov_P[key]+=$12
+}
+END{
+    for(i=1;i<=n;i++){
+        key=order[i]
+
+        pct_CS=(cov_CS[key]>0 ? 100*meth_CS[key]/cov_CS[key] : "NA")
+        pct_CSxP=(cov_CSxP[key]>0 ? 100*meth_CSxP[key]/cov_CSxP[key] : "NA")
+        pct_P=(cov_P[key]>0 ? 100*meth_P[key]/cov_P[key] : "NA")
+
+        print key,nsites[key],pct_CS,cov_CS[key],pct_CSxP,cov_CSxP[key],pct_P,cov_P[key]
+    }
+}'
+) | gzip > "$OUTPUT"
